5602b684a5a3e126e86cf90fa6e47fe449f6dfa2
Van Neves <vaneves@vaneves.com>
2015-10-22 15:06:29 -0300

    removal of unused imports

--- server.rb
+++ server.rb
@@ -1,11 +1,6 @@
 #!/usr/bin/env ruby
 
-require 'socket'
-require 'yaml'
 require './lib/clarity'
-require './lib/application'
-require './lib/request'
-require './lib/response'
 
 server = Clarity.new
 server.run
